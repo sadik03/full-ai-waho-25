df8499d2aed6095b15c0db3918741f2d342cb785
sadik03 <mssadik03@gmail.com>
2025-09-05 21:35:28 +0530

    change form page layout  h

--- src/components/ImageContainer.tsx
+++ src/components/ImageContainer.tsx
@@ -20,7 +20,7 @@ const ImageContainer: React.FC<ImageContainerProps> = ({ className = "" }) => {
           className="w-full max-w-[200px] sm:max-w-[250px] md:max-w-[300px] lg:max-w-[350px] 
                      h-auto min-h-[200px] sm:min-h-[250px] md:min-h-[300px] lg:min-h-[450px] 
                      max-h-[50vh] object-cover object-center 
-                     transition-all duration-500 hover:scale-105"
+                     transition-all duration-500 "
         />
       </motion.div>
     </div>
